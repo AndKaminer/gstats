a839ede8143e16ce0202808d99ff63b72e7564dd
Andrew Kaminer <akaminer@gatech.edu>
2026-06-17 12:23:47 -0700

    Add uint16_t big endian util

--- cpp/headers/util.hpp
+++ cpp/headers/util.hpp
@@ -10,7 +10,25 @@ inline uint32_t swap_endian32(uint32_t val) {
     ((val & 0x000000FF) << 24);
 }
 
-inline uint32_t read_big_endian_number(std::ifstream& file) {
+inline uint16_t swap_endian16(uint16_t val) {
+  return ((val & 0xFF00) >> 8) |
+    ((val & 0x00FF) << 8);
+}
+
+inline uint16_t read_big_endian_number16(std::ifstream& file) {
+  uint16_t to_return {0};
+  file.read(
+    reinterpret_cast<char*>(&to_return),
+    sizeof(to_return)
+  );
+
+  if constexpr (std::endian::native == std::endian::little) {
+    to_return = swap_endian16(to_return);
+  }
+  return to_return;
+}
+
+inline uint32_t read_big_endian_number32(std::ifstream& file) {
   uint32_t to_return {0};
   file.read(
     reinterpret_cast<char*>(&to_return),
